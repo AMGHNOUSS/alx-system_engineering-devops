fa2bdfc9807a88c7047e64e1f758fec9adea05ee
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-24 23:58:29 +0100

    Creat e File 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Client configuration file (w/ Puppet)
+
+file_line { 'Turn off passwd auth':
+	path => '/etc/ssh/ssh_config',
+	line => 'PasswordAuthentication no'
+}
+
+file_line { 'Declare identity file':
+	path => '/etc/ssh/ssh_config',
+	line => 'IdentityFile ~/.ssh/school'
+}
