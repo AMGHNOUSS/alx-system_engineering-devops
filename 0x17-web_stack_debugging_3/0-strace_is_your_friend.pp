8f6005fa72a6be72bb71560d07d37910a2f5b475
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-04-16 19:05:15 +0100

    Update File 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix problem of Apache returning 500 errors.
 
 exec { 'replace':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path => '/usr/local/bin/:/bin'
+	provider => shell,
+	command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
