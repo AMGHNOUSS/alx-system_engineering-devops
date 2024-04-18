cdedf3de948db6c05f770a6e84cc82f558f05f62
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-04-19 00:22:44 +0100

    Fix styling

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Fix the amouunt traffic a ngnix
 
 exec { 'fix':
-	command => 'sed -i "s/15/4096/" /etc/default/nginx',
-	path => '/usr/local/bin/:/bin'
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
 }
 
 exec { 'restart':
-	command => 'nginx restart',
-	path => '/etc/init.d/'
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
