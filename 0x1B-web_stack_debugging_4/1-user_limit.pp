4ec76b73f5ad28bdb39d1c0e63062149f0b8a193
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-04-19 00:24:25 +0100

    Fix styling

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Enable the user holbertoon to login and open files.
 
 exec { 'hard-file':
-	command => 'sed -i "/holbertoon hard/s/5/50000/" /etc/security/limits.conf',
-	path => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/holbertoon hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'soft-file':
-	command => 'sed -i "/holbertoon soft/s/4/50000/" /etc/security/limits.conf',
-	path => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/holbertoon soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
