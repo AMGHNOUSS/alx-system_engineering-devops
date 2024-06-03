e902b7f98eba1ec1bbff0ca95caff9b50b362555
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-06-04 00:31:26 +0100

    Fix error printing

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-# Gather information about Employee by passing a integer.
+"""
+Gather information about Employee by passing a integer.
+"""
 
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
+    # URL
     url = "https://jsonplaceholder.typicode.com/"
+
+    # Get data using API
     emp_id = sys.argv[1]
     response = requests.get(url + "users/" + emp_id)
     data = response.json()
     params = {"userId": emp_id}
     response_todos = requests.get(url + "todos", params)
     data_todos = response_todos.json()
+
+    # Handle with file csv.
     filename = emp_id + ".csv"
     with open(filename, mode='w', newline="") as file:
         for item in data_todos:
