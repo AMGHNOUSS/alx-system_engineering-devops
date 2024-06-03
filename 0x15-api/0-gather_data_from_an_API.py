6c41fc21c8a0ba0616af1455680b13bad2f4e5f0
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-06-03 22:35:15 +0100

    Create Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Gather information about Employee by passing a integer.
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    emp_id = sys.argv[1]
+
+    response = requests.get(url + "users/" + emp_id)
+    data = response.json()
+    params = {"userId": emp_id}
+    response_todos = requests.get(url + "todos", params)
+    data_todos = response_todos.json()
+
+    a = 0
+    for item in data_todos:
+        if item['completed'] is True:
+            a = a + 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(data['name'], a, len(data_todos)))
+    for item in data_todos:
+        if item['completed'] is True:
+            print("\t {}".format(item['title']))
