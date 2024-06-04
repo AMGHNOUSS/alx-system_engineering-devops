00b2bc32c665dd7238e00828fdb1be7594c25fc2
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-06-05 00:31:52 +0100

    coding style 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,11 +3,14 @@
 """
 Return the number of subscribes
 """
+
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    
+
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
 
