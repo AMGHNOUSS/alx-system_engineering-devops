3cc839c8e826ebcfe0bfaa7e2c34e6734af7cc0f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-06-06 22:08:09 +0100

    Create File 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
 
 """
-Return the number of subscribes
+Rerursive function: Return a list containing
+the titles of all hot articles.
 """
 
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after="", count=0):
 
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
 
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    params = {
+            "after": after,
+            "count": count,
+            "limit": 100
+            }
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirect=False)
 
     if (response.status_code) == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
+        results = response.json().get("data")
+        after = results.get("after")
+        count += results.get("dist")
+        for c in results.get("children"):
+            hot_list.append(c.get("data").get("title"))
+        if after is not None:
+            return recurse(subreddit, hot_list, after, count)
+        return hot_list
     else:
-        return 0
+        return None
